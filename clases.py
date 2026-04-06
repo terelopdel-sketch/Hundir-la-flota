0a22407c078fb22bc47fa75eb99c981fcf460439
raquelmg1312 <raquelmg1312@gmail.com>
2026-04-06 21:01:59 +0200

    class tablero primera parte

--- clases.py
+++ clases.py
@@ -1,1 +1,15 @@
 
+Class Tablero:
+
+# Define el nombre del jugador, tamaño del tablero, el tablero del jugador1 y jugador 2
+
+def __init__(self, player_id: str):
+    self.player_id = player_id
+    self.size = BOARD_SIZE
+    self.board = np.full((self.size, self.size), EMPTY)
+    self.tracking = np.full((self.size, self.size), EMPTY)
+
+# Coloca barcos de forma aleatoria en el tablero del jugador
+
+
+
